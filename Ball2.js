f9695c62d686fe1f35594f0e9de7900412a8368d
Aishani2002 <61320981+Aishani2002@users.noreply.github.com>
2020-04-04 19:41:26 +0530

    Add files via upload

---
+++ Ball2.js
@@ -0,0 +1,7 @@
+class Ball2 extends Ball {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("Pics/ball2.png");
+    }
+  
+  };
---
+++ Redblock.js
@@ -0,0 +1,22 @@
+class Redblock extends Blocks {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("Pics/red block.png");
+    this.Visiblity = 255;
+  }
+display () {
+
+  if (this.body.speed<3){
+  super.display();
+}
+
+else {
+  push();
+World.remove(world,this.body);
+this.Visiblity = this.Visiblity-5;
+tint(255,this.Visiblity);
+image(this.image,this.body.position.x,this.body.position.y,50,50);
+pop();
+}
+}
+};
